e6b8647105d0b51b1c10614343f597ec688e3ce2
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-04-11 23:59:40 +0630

    HttpClient Test

---
+++ C_Sharp_Basic/HttpClient/HttpClientTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace C_Sharp_Basic;
+
+public class HttpClientTest
+{
+    public static async void Run()
+    {
+        try
+        {
+            using var client = new HttpClient();
+
+            using var response = await client.GetAsync("https://www.google.com");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responeBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responeBody);
+            }
+            else
+            {
+                Console.WriteLine("Fetch Error");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+    }
+}
--- C_Sharp_Basic/Program.cs
+++ C_Sharp_Basic/Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using C_Sharp_Basic;
 using System.Collections;
 using System.Xml.Linq;
 
+HttpClientTest.Run();
+
+Console.ReadLine();
