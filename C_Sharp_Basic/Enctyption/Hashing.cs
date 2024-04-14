01fa65c70c1d8ade3154541c20d8b107d8f2bf22
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-04-15 00:29:31 +0630

    SHA Hashing Complete.

--- C_Sharp_Basic/Enctyption/Hashing.cs
+++ C_Sharp_Basic/Enctyption/Hashing.cs
@@ -18,12 +18,20 @@ public class Hashing
 
         //var watch = Stopwatch.StartNew();
         var result = HashUsingMD5(text).Result;
-        var resutl1 = HashUsingMD5(text1).Result;
+        var result1 = HashUsingMD5(text1).Result;
         
         Console.WriteLine($"Hashed value of 'Hello World' => \n {result} \n length is => {result.Length}");
-        Console.WriteLine($"Hashed value of 'var strStreamOne = new MemoryStream(Encoding.UTF8.GetBytes(text));' => \n {resutl1} \n length is {resutl1.Length}");
+        Console.WriteLine($"Hashed value of 'var strStreamOne = new MemoryStream(Encoding.UTF8.GetBytes(text));' => \n {result1} \n length is {result1.Length}");
         //watch.Stop();
         //Console.WriteLine($"Elapsed Time is => {watch.Elapsed.TotalMilliseconds}");
+
+        Console.WriteLine("\n ---- Using SHA -------"); 
+        var ShaResult = HashUsingMD5(text).Result;
+        var ShaResult1 = HashUsingMD5(text1).Result;
+
+        Console.WriteLine($"Hashed value of 'Hello World' => \n {ShaResult} \n length is => {ShaResult.Length}");
+        Console.WriteLine($"Hashed value of 'var strStreamOne = new MemoryStream(Encoding.UTF8.GetBytes(text));' => \n {ShaResult1} \n length is {ShaResult1.Length}");
+
     }
 
     private static async Task<string> HashUsingMD5(string text)
@@ -35,6 +43,21 @@ public class Hashing
         byte[] hashOne;
 
         using var hasher = MD5.Create();
+        hashOne = await hasher.ComputeHashAsync(strStreamOne);
+        hashedResutl = Convert.ToBase64String(hashOne);
+
+        return hashedResutl;
+    }
+
+    private static async Task<string> HashUsingSHA(string text)
+    {
+        var hashedResutl = string.Empty;
+
+        var strStreamOne = new MemoryStream(Encoding.UTF8.GetBytes(text));
+
+        byte[] hashOne;
+
+        using var hasher = SHA256.Create();
         hashOne = await hasher.ComputeHashAsync(strStreamOne);
         hashedResutl = Convert.ToBase64String(hashOne);
 
