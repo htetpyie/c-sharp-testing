1fef45cf74a2ed4cf8b00fb609e65cfa98a4d645
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-04-16 22:04:51 +0630

    Complete AES Encryption

--- C_Sharp_Basic/Program.cs
+++ C_Sharp_Basic/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using C_Sharp_Basic;
+using C_Sharp_Basic.Enctyption;
 using System.Collections;
 using System.Xml.Linq;
 
 //HttpClientTest.Run();
-Hashing.Run();
+EncryptionExample.Run();
 
 Console.ReadLine();
