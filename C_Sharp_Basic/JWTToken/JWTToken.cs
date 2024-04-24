edcde5e83d327409ac3d765db18dc3f1465d13a7
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-04-24 22:52:43 +0630

    Token Generation Complete

---
+++ C_Sharp_Basic/JWTToken/JWTToken.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Basic;
+
+public class JWTToken
+{
+
+    public static void Run()
+    {
+        string UserId = "TETTTTT-0001-0001";
+        GenerateToken(UserId);
+    }
+
+    public static string GenerateToken(string userId)
+    {
+        var secret = "ERMN05OPLoDvbTTa/QkqLNMI7cPLguaRyHzyg7n5qNBVjQmtBhz4SzYh4NBVCXi3KJHlSXKP+oi2+bXr6CUYTR==";
+        byte[] key = Convert.FromBase64String(secret);
+        SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+        SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[] {
+                  new Claim(ClaimTypes.Name, userId), new Claim(ClaimTypes.Role, "0")}),
+            Expires = DateTime.Now.AddDays(5),
+            SigningCredentials = new SigningCredentials(securityKey,
+            SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+        JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+        var result =  handler.WriteToken(token);
+        Console.WriteLine(result);
+        return result;
+    }
+}
--- C_Sharp_Basic/Program.cs
+++ C_Sharp_Basic/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using C_Sharp_Basic;
 using System.Collections;
+using System.IdentityModel.Tokens.Jwt;
 using System.Xml.Linq;
 
 //HttpClientTest.Run();
-EncryptionExample.RunDSA();
+//EncryptionExample.RunDSA();
+
+JWTToken.Run();
 
 Console.ReadLine();
