3fdf42f38099886ec9e426a2d0e2a36532f6296e
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-06-01 18:29:17 +0630

    -

--- APIApp/Controllers/v1/WeatherForecastController.cs
+++ APIApp/Controllers/v1/WeatherForecastController.cs
@@ -11,7 +11,6 @@ namespace APIApp.Controllers.v1
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
