16810425636bbc4c8e5ca2f1b4ebe93c92200043
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2024-06-06 00:11:35 +0630

    -

--- APIApp/Program.cs
+++ APIApp/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers(option =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 #region Graphql
 builder.Services
@@ -53,8 +53,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     // Enable the Playground middleware
     //app.UsePlayground();
