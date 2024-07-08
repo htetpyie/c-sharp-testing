a2041a77d96eef831bb8731bc906476588a0889c
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-07-08 19:02:56 +0630

    -

--- APIApp/GraphQl/Query.cs
+++ APIApp/GraphQl/Query.cs
@@ -5,10 +5,10 @@ namespace APIApp.GraphQl
     public class Query
     {
         private readonly List<Product> _products = new()
-    {
-        new Product { Id = 1, Name = "Laptop", Price = 999.99m },
-        new Product { Id = 2, Name = "Smartphone", Price = 799.99m },
-    };
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 999.99m },
+            new Product { Id = 2, Name = "Smartphone", Price = 799.99m },
+        };
 
         public List<Product> GetProducts() => _products;
     }
