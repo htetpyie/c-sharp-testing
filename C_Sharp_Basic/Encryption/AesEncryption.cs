ddceef2d13ce661e1a68837dddc55c8ebc9be208
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-08-02 14:32:39 +0630

    Aes enctyption/ decryption test completed

---
+++ C_Sharp_Basic/Encryption/AesEncryption.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Encryption
+{
+	//https://learn.microsoft.com/en-us/dotnet/api/system.security.cryptography.aes?view=net-8.0
+	public class AesEncryption
+	{
+		public static void Run()
+		{
+			string original = "Here is some data to encrypt! မောင်မောင် မမ ထထက ဋ အ ဉီးစားပေးပါ။";
+
+			using var myAes = Aes.Create();
+			byte[] encrypted = EncryptStringToBytes_Aes(original, myAes.Key, myAes.IV);
+			var decrypted = DecryptStringFromBytes_Aes(encrypted, myAes.Key, myAes.IV);
+			Console.WriteLine($"Original String => {original}");
+			Console.WriteLine($"Encrypted String => {Convert.ToBase64String(encrypted)}");
+			Console.WriteLine($"Decrypted String => {decrypted}");
+		}
+
+		public static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+		{
+			// Check Arguments
+			if (plainText.IsNullOrEmpty())
+				throw new ArgumentNullException("plainText");
+			if (Key.IsNullOrEmpty())
+				throw new ArgumentNullException("Key");
+			if (IV.IsNullOrEmpty())
+				throw new ArgumentNullException("IV");
+			byte[] encrypted;
+
+			//Create an Aes Object
+			//with the specified key and IV
+			var aesAlg = Aes.Create();
+			aesAlg.Key = Key;
+			aesAlg.IV = IV;
+
+			//Create an encryptor to perform the stream transform.
+			ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+			//Create the streams used for encryption.
+			using (MemoryStream msEncrypt = new MemoryStream())
+			{
+				using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+				{
+					using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+					{
+						//Write all data to the stream.
+						swEncrypt.Write(plainText);
+					}
+					encrypted = msEncrypt.ToArray();
+				}
+			}
+
+			return encrypted;
+		}
+
+		public static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+		{
+			// Check Arguments
+			if(cipherText.IsNullOrEmpty()) throw new ArgumentNullException("Text");
+			if (Key.IsNullOrEmpty()) throw new ArgumentNullException("Key");
+			if (IV.IsNullOrEmpty()) throw new ArgumentNullException("IV");
+
+			//Declare the string used to hold
+			// the decrypted text.
+			string plainText = null;
+
+			using (Aes aesAlg = Aes.Create())
+			{
+				aesAlg.Key = Key;
+				aesAlg.IV = IV;
+
+				// Create a decryptor to perform the stream transform.
+				ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+				// Create the streams used for decryption.
+				using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+				{
+					using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+					{
+						using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+						{
+
+							// Read the decrypted bytes from the decrypting stream
+							// and place them in a string.
+							plainText = srDecrypt.ReadToEnd();
+						}
+					}
+				}
+			}
+
+			return plainText;
+		}
+	}
+}
--- C_Sharp_Basic/Program.cs
+++ C_Sharp_Basic/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Basic.Encryption;
 using C_Sharp_Basic;
 using C_Sharp_Basic.PDFGeneration.PDFSharpCore;
 using System.Collections;
 using System.IdentityModel.Tokens.Jwt;
 using System.Xml.Linq;
 
+AesEncryption.Run();
 //HttpClientTest.Run();
 //EncryptionExample.RunDSA();
 //new PDFExamples().GenerateUsingPDFSharpCore();
