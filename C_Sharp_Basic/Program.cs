b2999fb4618fc1a7870f3eb0b19edb3ebaed285b
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-09-21 23:32:24 +0630

    feat : add qr generator and test

--- C_Sharp_Basic/Program.cs
+++ C_Sharp_Basic/Program.cs
@@ -1,12 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Basic.Encryption;
-using C_Sharp_Basic;
-using C_Sharp_Basic.PDFGeneration.PDFSharpCore;
-using System.Collections;
-using System.IdentityModel.Tokens.Jwt;
-using System.Xml.Linq;
+using Basic.QRTest;
 
-AesEncryption.Run();
+//AesEncryption.Run();
+QRGeneratorService.GenerateQR();
 //HttpClientTest.Run();
 //EncryptionExample.RunDSA();
 //new PDFExamples().GenerateUsingPDFSharpCore();
---
+++ C_Sharp_Basic/QRTest/QRGeneratorService.cs
@@ -0,0 +1,26 @@
+﻿using QRCoder;
+
+namespace Basic.QRTest;
+
+public class QRGeneratorService
+{
+	public static void GenerateQR()
+	{
+		using var qrGenerator = new QRCodeGenerator();
+		using QRCodeData qrCodeData = qrGenerator
+			.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+		using PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+
+		byte[] qrCodeImage = qrCode.GetGraphic(20);
+
+
+		var base64String = Convert.ToBase64String(qrCodeImage);
+		var byteARr = Convert.FromBase64String(base64String);
+
+		//write to file
+		var filePath = Directory.GetCurrentDirectory() + "/QRImage.png";
+		File.WriteAllBytes(filePath, qrCodeImage);
+
+		Console.WriteLine(base64String);
+	}
+}
