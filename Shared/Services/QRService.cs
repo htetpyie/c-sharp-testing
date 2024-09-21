da7628a1d09af15d38946b903e56b6b03bbc73d2
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-09-21 23:41:37 +0630

    feat : QR code service

---
+++ Shared/Services/QRService.cs
@@ -0,0 +1,18 @@
+﻿using QRCoder;
+
+namespace Shared.Services;
+public class QRService
+{
+	public byte[] GenerateQR(string text)
+	{
+		using var qrGenerator = new QRCodeGenerator();
+		using QRCodeData qrCodeData = qrGenerator
+			.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+		using PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+		//using QRCode qr = new QRCode(qrCodeData);
+
+		byte[] qrCodeImage = qrCode.GetGraphic(20);
+
+		return qrCodeImage;
+	}
+}
