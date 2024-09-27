37e283a94d14361a347ee7d35fe40cd885d1b8a7
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-09-28 00:43:16 +0630

    feat : transaction tested

--- Service/Class/ClassService.cs
+++ Service/Class/ClassService.cs
@@ -2,31 +2,32 @@
 
 namespace Service.ClassService
 {
-    public class ClassService
-    {
-        private readonly SQLAppDbContext _sqlDb;
+	public class ClassService
+	{
+		private readonly SQLAppDbContext _sqlDb;
 
-        public ClassService(SQLAppDbContext sqlDb)
-        {
-            _sqlDb = sqlDb;
-        }
+		public ClassService(SQLAppDbContext sqlDb)
+		{
+			_sqlDb = sqlDb;
+		}
 
-        public async Task InsertStudent()
-        {
-            var classList = new List<TblClass>();
-            foreach (var item in Enumerable.Range(1, 10))
-            {
-                var classItem = new TblClass
-                {
-                    Name = $"Class {item}"
-                };
-                classList.Add(classItem);
-            }
+		public async Task InsertStudent()
+		{
+			var classList = new List<TblClass>();
+			using var transaction = await _sqlDb.Database.BeginTransactionAsync();
+			foreach (var item in Enumerable.Range(1, 10))
+			{
+				var classItem = new TblClass
+				{
+					Name = $"Class {item}"
+				};
+				classList.Add(classItem);
+			}
 
-            await _sqlDb.AddRangeAsync(classList);
-            await _sqlDb.SaveChangesAsync();
-
-            var batch = new TblBatch();
-        }
-    }
+			await _sqlDb.AddRangeAsync(classList);
+			await _sqlDb.SaveChangesAsync();
+			await transaction.RollbackAsync();
+			var batch = new TblBatch();
+		}
+	}
 }
