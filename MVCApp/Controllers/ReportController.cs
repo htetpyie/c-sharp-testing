8bd842edb8089eb1407e66fa4b1eeeab9473f6eb
Htet Pyie Phyoe Maung <72303651+htetpyie@users.noreply.github.com>
2024-12-05 11:35:53 +0630

    feat : RDLC report with error

--- MVCApp/Controllers/ReportController.cs
+++ MVCApp/Controllers/ReportController.cs
@@ -46,7 +46,7 @@ namespace MVCApp.Controllers
 		}
 
 		#region RDLC
-		public IActionResult Print()
+		public IActionResult RDLC()
 		{
 			try
 			{
@@ -54,7 +54,7 @@ namespace MVCApp.Controllers
 				int extnesion = 1;
 
 				var jsonFilePath = "E:\\HPPM\\Projects\\c-sharp-testing\\Shared\\Jsons\\blog_data.json";
-				var reportPath = $"{this._webHostEnvironment.WebRootPath}\\Reports\\Blog.rdlc";
+				var reportPath = $"{_webHostEnvironment.WebRootPath}\\Reports\\Blog.rdlc";
 
 				var parameters = new Dictionary<string, string>();
 
@@ -64,14 +64,13 @@ namespace MVCApp.Controllers
 
 				parameters.Add("title", blog.Title);
 				parameters.Add("author", blog.Author);
-				LocalReport localReport = new LocalReport(reportPath);
+				var localReport = new LocalReport(reportPath);
 
-				var result = localReport.Execute(RenderType.Pdf, extnesion, parameters, mimtype);
+				var result = localReport.Execute(RenderType.Pdf, extnesion, parameters);
 				return File(result.MainStream, "application/pdf");
 			}
 			catch (Exception ex)
 			{
-
 				throw;
 			}
 		}
